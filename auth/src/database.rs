af7f0b6f6e33b2191be9cd9773f0dcf0ecc52b7d
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-10-28 10:40:41 +0100

    chore: add databse manager

--- auth/src/database.rs
+++ auth/src/database.rs
@@ -1,9 +1,60 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use uuid::Uuid;
-#[derive(Debug, Serialize)]
+#[derive(Debug)]
 
+/// the database is reusable outside to module
+/// especially for sharing the database connection
+/// with other modules
+pub struct Database {}
+
+impl Database {
+    pub async fn conn() -> Pool<Postgres> {
+        let database_connection_url =
+            std::env::var("DATABASE_URL").expect("error parsing DATABASE_URL");
+
+        let database_pool_connection = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&database_connection_url)
+            .await
+            .expect("error creating connection");
+
+        database_pool_connection
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+#[serde(rename_all = "camelCase")]
 pub struct UserInformation {
     id: Uuid,
-    username: String,
+    email: String,
     password: String,
+    is_verified: bool,
+}
+
+pub struct UserInformationBuilder<'a>(&'a str, &'a str);
+impl<'a> UserInformationBuilder<'a> {
+    pub fn new(email: &'a str, password: &'a str) -> Self {
+        Self(email, password)
+    }
+}
+
+impl UserInformation {
+    pub async fn new(user: UserInformationBuilder<'_>) -> Self {
+        let database_pool_connection = Database::conn().await;
+        let user = sqlx::query_as::<_, UserInformation>(
+            r#"
+            INSERT INTO user_information (email, password)
+            VALUES ($1, $2)
+            RETURNING id, email, password, is_verified
+            "#,
+        )
+        .bind(user.0)
+        .bind(user.1)
+        .fetch_one(&database_pool_connection)
+        .await
+        .expect("error creating user");
+
+        user
+    }
 }
