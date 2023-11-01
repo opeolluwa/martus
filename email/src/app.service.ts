586deccb559d0c816201c1abf80990141a4b9ce6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-11-01 01:09:35 +0100

    chore: add kafkajs

--- email/src/app.service.ts
+++ email/src/app.service.ts
@@ -1,7 +1,9 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly mailerService: MailerService) {}
   getHello(): string {
     return 'Hello World!';
   }
