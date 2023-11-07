97b62dd009e739bcea01faa6e41764f41830320d
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-11-08 00:33:44 +0100

    chore: add kafka

--- email/src/app.module.ts
+++ email/src/app.module.ts
@@ -4,23 +4,32 @@ import { AppService } from './app.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 import { KafkaModule } from './kafka/kafka.module';
+import { NodemailerModule } from './nodemailer/nodemailer.module';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
-      defaults: {
-        from: '"Martus Team" <modules@nestjs.com>',
-      },
-      template: {
-        dir: __dirname + '/templates',
-        adapter: new EjsAdapter(),
-        options: {
-          strict: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: configService.get<string>('SMTP_TRANSPORTER'),
+        defaults: {
+          from: configService.get<string>('SMTP_USERNAME'),
         },
-      },
+        template: {
+          dir: join(__dirname, '..', 'templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
     }),
     KafkaModule,
+    NodemailerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- email/src/app.service.ts
+++ email/src/app.service.ts
@@ -1,22 +1,12 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import {
-  Injectable,
-  OnApplicationShutdown,
-  OnModuleInit,
-} from '@nestjs/common';
-import {
-  Consumer,
-  ConsumerRunConfig,
-  ConsumerSubscribeTopics,
-  Kafka,
-} from 'kafkajs';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { KafkaService } from './kafka/kafka.service';
+import { NodemailerService } from './nodemailer/nodemailer.service';
 
 @Injectable()
 export class AppService implements OnModuleInit {
   constructor(
     private readonly kafkaService: KafkaService,
-    private readonly mailerService: MailerService,
+    private readonly mailer: NodemailerService,
   ) {}
 
   async onModuleInit() {
@@ -24,29 +14,14 @@ export class AppService implements OnModuleInit {
       { topics: ['email-queue'] },
       {
         eachMessage: async ({ topic, partition, message }) => {
+          const value = message.value.toString();
+          console.log(`Received message ${value} from topic ${topic}`);
+          this.mailer.sendEmail(JSON.parse(value));
           console.log({
             value: message.value.toString(),
             topic: topic.toString(),
             partition: partition.toString(),
           });
-
-          // await this.mailerService
-          // .sendMail({
-          // to: recipient, // the receiver email, parsed from the job.data
-          // from: '"Easepay" <info@easepay.io>', // sender address
-          // subject: emailSubject, // Subject line
-          // template: emailTemplate, // the email template to use  eg welcome, sign up, forgotten-password
-          // context: {
-          // fullname: data?.fullname || data?.name, // if the email require email
-          // token: data?.token, // if the email contain JWT or OTP
-          // unsubscribeLink, // link to unsubscribe from emails
-          // contactUsPage, // contact up page link
-          // data, // make the rest of the data accessible in the template as data[field]
-          // }, // data to be used to saturate the template
-          // })
-          // .catch((error) => {
-          // throw new Error(error);
-          // });
         },
       },
     );
