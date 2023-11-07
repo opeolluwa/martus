343cb4f005e127ee035c6bf86ae4de9559693fd4
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-11-08 00:33:18 +0100

    feat: config mailer

--- email/src/nodemailer/nodemailer.module.ts
+++ email/src/nodemailer/nodemailer.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NodemailerService } from './nodemailer.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  providers: [NodemailerService]
+  providers: [NodemailerService],
+  exports: [NodemailerService],
+  imports: [MailerModule],
 })
 export class NodemailerModule {}
